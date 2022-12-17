334910e2cefac8f09739814403cfd4b516080d97
Evans <ellaboevans@gmail.com>
2022-12-17 03:44:09 +0000

    First Commit
    Started the setup on the local dir

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import logo from "./assets/tesla-logo.png";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center h-80">
+      <img
+        src={logo}
+        alt=""
+        className="w-4/6 h-4/6 object-contain -mb-16 mt-64"
+      />
+      <div className="m-8">
+        <h1 className="text-center font-semibold text-3xl font-Gotham ">
+          Yet to start some{" "}
+          <span className="uppercase tracking-wider text-red-600 underline">
+            tesla
+          </span>{" "}
+          side project
+        </h1>
+        <p className="font-Gotham tracking-wide  text-center text-xl mt-8 bg-gray-400 py-8  rounded shadow-lg ">
+          Do some counting
+          <span className="font-bold text-3xl block text-red-600">{count}</span>
+        </p>
+      </div>
+      <button
+        onClick={handleClick}
+        className="py-4 px-8 rounded-lg text-white bg-blue-500  hover:text-red-500 transition-all duration-200 "
+      >
+        Let's start counting
+      </button>
+      <p className="mt-8">
+        <small>
+          Powered <strong>CodeConcept</strong>
+        </small>
+      </p>
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Gotham: ["Gotham", "sans-serif"],
+        gothamBlack: ["Gotham Black", "sans-serif"],
+        gothamBook: ["Gotham Book", "sans-serif"],
+        gothamUltra: ["Gotham Ultra", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
