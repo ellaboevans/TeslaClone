f1e0c1c41578b1ce37f3a69ad136fe7f92439b03
Evans <ellaboevans@gmail.com>
2022-12-18 17:41:59 +0000

    Rename Components directory

---
+++ src/Components/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Button({
+  children,
+  btnColor = "",
+  processing,
+  type = "button",
+}) {
+  return (
+    <button
+      type={type}
+      className={
+        ` ${btnColor} inline-flex items-center justify-center w-60 h-10 rounded text-sm capitalize font-bold ${
+          processing
+        }`
+      }
+      disabled={processing}
+    >
+      {children}
+    </button>
+  );
+}
