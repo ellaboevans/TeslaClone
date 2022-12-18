4db640da677e5e4ae62ca30ddbdab70620871d3e
Evans <ellaboevans@gmail.com>
2022-12-18 19:33:59 +0000

    Fix Hero section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,8 +5,8 @@ import Button from "./Button";
 
 export default function Header() {
   return (
-    <section className="md:bg-hero-image-desktop bg-hero-image-mobile bg-no-repeat lg:bg-top md:bg-center h-screen bg-cover">
-      <header className="p-4 font-medium text-gray-700 text-sm  font-Gotham">
+    <section className="p-2 md:bg-hero-image-desktop bg-hero-image-mobile bg-no-repeat lg:bg-top md:bg-center h-screen bg-cover overflow-hidden">
+      <header className="p-4 font-medium text-gray-700 text-sm font-Gotham">
         <nav className="flex justify-between items-center">
           <div id="logo" className="w-2/12 cursor-pointer">
             <img src={logo} alt="" />
@@ -70,21 +70,21 @@ export default function Header() {
           </div>
         </nav>
       </header>
-      <div className="flex flex-col h-3/4 items-center justify-around ">
-        <div id="hero-tex" className=" text-center font-Gotham m-40 p-8">
+      <div className="flex flex-col h-3/4 items-center py-24">
+        <div id="hero-tex" className=" text-center font-Gotham mt- p-8">
           <h1 className="text-5xl font-medium mb-2 text-gray-800">Model 3</h1>
           <a href="#" className="text-sm text-gray-700 underline">
             Schedule a Test Drive
           </a>
         </div>
-        <div className="flex justify-between gap-4 items-center mt-32 ">
+        <div className="flex justify-between gap-4 items-center p-7 mt-28 ">
           <Button btnColor="bg-btn-bg text-white backdrop-blur">Buy Now</Button>
           <Button btnColor="bg-white text-btn-bg backdrop-blur opacity-80">
             Customer Order
           </Button>
         </div>
-        <div className="w-12 h-12 py-16 mt-1">
-          <img src={chevron} alt="" className="w-full animate-bounce" />
+        <div className="w-12 h-16 py-16 mt-0">
+          <img src={chevron} alt="" className="w-full mt-8 animate-bounce" />
         </div>
       </div>
     </section>
