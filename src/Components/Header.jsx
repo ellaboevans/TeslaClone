fa32e5fbb55cd061fbb8fb70669446d91ceaca10
Evans <ellaboevans@gmail.com>
2022-12-18 23:06:30 +0000

    Fix Hero Section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,13 +5,13 @@ import Button from "./Button";
 
 export default function Header() {
   return (
-    <section className="p-2 md:bg-hero-image-desktop bg-hero-image-mobile bg-no-repeat lg:bg-top md:bg-center h-screen bg-cover overflow-hidden">
+    <section className="p-2 md:bg-hero-image-desktop bg-hero-image-mobile bg-no-repeat  md:bg-center h-screen bg-cover overflow-hidden">
       <header className="px-8 font-medium text-gray-700 text-sm font-Gotham">
         <nav className="flex justify-between items-center">
-          <div id="logo" className="w-36 cursor-pointer">
+          <div id="logo" className="w-36 -mt-4 cursor-pointer">
             <img src={logo} alt="" className="object-contain w-full" />
           </div>
-          <ul className="hidden md:flex flex items-center gap-1">
+          <ul className="hidden md:flex flex items-center -mt-4 gap-1">
             <a href="#">
               <li className="hover:bg-slate-400 backdrop-blur inline-flex transition esase-out duration-700 py-1 px-4  cursor-pointer rounded">
                 Model S
@@ -70,16 +70,16 @@ export default function Header() {
           </div>
         </nav>
       </header>
-      <div className="flex flex-col h-3/4 items-center py-20 md:py-5">
+      <div className="flex flex-col h-3/4 items-center py-20 md:py-0">
         <div id="hero-tex" className=" text-center font-Gotham p-8 md:p-8">
-          <h1 className="text-5xl md:text-7xl font-medium text-gray-800">
+          <h1 className="text-4xl md:-mt-6 font-medium text-gray-800">
             Model 3
           </h1>
           <a href="#" className="text-sm text-gray-700 underline">
             Schedule a Test Drive
           </a>
         </div>
-        <div className="flex flex-col md:flex-row justify-between gap-4 items-center md:p-11 p-4 mt-36 ">
+        <div className="flex flex-col md:flex-row justify-between gap-4 items-center md:p-11 p-4 mt-52   ">
           <a href="#">
             <Button btnColor="bg-btn-bg text-white backdrop-blur opacity-95">
               Buy Now
