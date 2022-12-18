0684bed66bd9f1018cce312ea9f6042c4bf1f03a
Evans <ellaboevans@gmail.com>
2022-12-18 22:47:25 +0000

    Fix background on Menu link for mobile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
     <section className="p-2 md:bg-hero-image-desktop bg-hero-image-mobile bg-no-repeat lg:bg-top md:bg-center h-screen bg-cover overflow-hidden">
       <header className="px-8 font-medium text-gray-700 text-sm font-Gotham">
         <nav className="flex justify-between items-center">
-          <div id="logo" className="w-36 utline cursor-pointer">
-            <img src={logo} alt="" className="object-con" />
+          <div id="logo" className="w-36 cursor-pointer">
+            <img src={logo} alt="" className="object-contain w-full" />
           </div>
           <ul className="hidden md:flex flex items-center gap-1">
             <a href="#">
@@ -51,26 +51,26 @@ export default function Header() {
           <div className=" flex gap-1">
             <a
               href="#"
-              className="hover:bg-slate-400 backdrop-blur inline-flex transition esase-out duration-700 py-1 px-4 rounded"
+              className="hover:bg-slate-400 backdrop-blur hidden md:inline-flex transition esase-out duration-700 py-1 px-4 rounded"
             >
               Shop
             </a>
             <a
               href="#"
-              className="hover:bg-slate-400 backdrop-blur inline-flex transition esase-out duration-700 py-1 px-4 rounded"
+              className="hover:bg-slate-400 backdrop-blur hidden md:inline-flex transition esase-out duration-700 py-1 px-4 rounded"
             >
               Account
             </a>
             <a
               href="#"
-              className="hover:bg-slate-400 backdrop-blur inline-flex transition esase-out duration-700 py-1 px-4 rounded"
+              className="hover:bg-slate-400 bg-slate-400 md:bg-transparent backdrop-blur inline-flex transition esase-out duration-700 py-1 px-4 rounded"
             >
               Menu
             </a>
           </div>
         </nav>
       </header>
-      <div className="flex flex-col h-3/4 items-center py-20 md:py-16">
+      <div className="flex flex-col h-3/4 items-center py-20 md:py-5">
         <div id="hero-tex" className=" text-center font-Gotham p-8 md:p-8">
           <h1 className="text-5xl md:text-7xl font-medium text-gray-800">
             Model 3
@@ -79,7 +79,7 @@ export default function Header() {
             Schedule a Test Drive
           </a>
         </div>
-        <div className="flex flex-col md:flex-row justify-between gap-4 items-center md:p-11 p-4 mt-40 ">
+        <div className="flex flex-col md:flex-row justify-between gap-4 items-center md:p-11 p-4 mt-36 ">
           <a href="#">
             <Button btnColor="bg-btn-bg text-white backdrop-blur opacity-95">
               Buy Now
@@ -91,7 +91,7 @@ export default function Header() {
             </Button>
           </a>
         </div>
-        <div className="w-12 h-16 md:py-0 -pt-1">
+        <div className="w-12 h-16 pb-8">
           <img src={chevron} alt="" className="w-full animate-bounce" />
         </div>
       </div>
