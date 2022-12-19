2a6ac7dd1f2bb62430e05cc51b3c2b0f24630192
Evans <ellaboevans@gmail.com>
2022-12-19 00:48:01 +0000

    Configure Background Images for ModelS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,8 @@ module.exports = {
         "hero-image-mobile": "url('./assets/M3-Homepage-Mobile-LHD.jpeg')",
         "modely-desktop": "url('./assets/Desktop-ModelY.jpeg')",
         "modely-mobile": "url('./assets/Mobile-ModelY.jpeg')",
+        "model-s-desktop": "url('./assets/Desktop-ModelS.jpeg')",
+        "model-s-mobile": "url('./assets/Mobile-ModelS.jpeg')",
       },
       colors: {
         "btn-bg": "rgba(23,26,32,0.8)",
