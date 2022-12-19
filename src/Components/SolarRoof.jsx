ceba5fb715347e0fda3f177e15c23caf14e10b7e
Evans <ellaboevans@gmail.com>
2022-12-19 01:27:53 +0000

    Add SolarRoof Component to the dir

---
+++ src/Components/SolarRoof.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import Button from "./Button";
+
+export default function SolarRoof(){
+    return (
+      <section className="p-2 md:bg-solarPanel-desktop bg-solarPanel-mobile bg-no-repeat  md:bg-center h-screen bg-cover overflow-hidden">
+        <div className="flex flex-col h-3/4 items-center mt-20 py-20 md:py-0">
+          <div id="hero-tex" className=" text-center font-Gotham p-8 md:p-8">
+            <h1 className="text-4xl md:-mt-6 font-medium text-gray-800">
+              Solar Roof
+            </h1>
+            <a href="#" className="text-sm text-gray-700">
+              Produce Clean Enegy From Your Roof
+            </a>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between gap-4 items-center md:p-11 p-4 mt-44 md:mt-72   ">
+            <a href="#">
+              <Button btnColor="bg-btn-bg text-white backdrop-blur opacity-95">
+                Order Now
+              </Button>
+            </a>
+            <a href="#">
+              <Button btnColor="bg-white text-btn-bg backdrop-blur opacity-90">
+                Learn More
+              </Button>
+            </a>
+          </div>
+        </div>
+      </section>
+    );
+}
