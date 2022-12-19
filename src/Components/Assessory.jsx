b1108588e8dcc71fb9535feab0ceeb00f2102509
Evans <ellaboevans@gmail.com>
2022-12-19 02:59:06 +0000

    Import Footer Component

--- src/Components/Assessory.jsx
+++ src/Components/Assessory.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import Footer from "./Footer";
 
 export default function Assessory() {
   return (
@@ -10,41 +11,16 @@ export default function Assessory() {
             Accessories
           </h1>
         </div>
-        <div className="flex flex-col md:flex-row justify-between gap-4 items-center mb-12 md:p-11 p-4 mt-60 md:mt-60   ">
+        <div className="flex flex-col md:flex-row justify-between gap-4 items-center mb-12 md:p-11 p-4 mt-56 md:mt-60   ">
           <a href="#">
             <Button btnColor="bg-btn-bg text-white backdrop-blur opacity-100">
               Shop Now
             </Button>
           </a>
         </div>
-        <div className="flex md:flex-row flex-col text-center items-center md:gap-6 text-gray-600">
-          <a href="#">
-            <small>
-              Tesla &#169; <span>2022</span>
-            </small>
-          </a>
-          <a href="#">
-            <small>Privacy & Legal</small>
-          </a>
-          <a href="#">
-            <small>Vehicle Recalls</small>
-          </a>
-          <a href="#" className="hidden md:inline-flex">
-            <small>Contact</small>
-          </a>
-          <a href="#">
-            <small>Careers</small>
-          </a>
-          <a href="#">
-            <small>News</small>
-          </a>
-          <a href="#" className="hidden md:inline-flex">
-            <small>Engage </small>
-          </a>
-          <a href="#" className="hidden md:inline-flex">
-            <small>Locations</small>
-          </a>
-        </div>
+      </div>
+      <div className="flex justify-center -mt-8">
+        <Footer />
       </div>
     </section>
   );
