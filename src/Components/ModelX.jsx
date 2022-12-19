6cea7ccaaf6dded32b4a6c9ae591a0bca54e28c0
Evans <ellaboevans@gmail.com>
2022-12-19 01:11:19 +0000

    Add Model-X components
    Also add Background Images for dispaly on section

---
+++ src/Components/ModelX.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "./Button";
+
+export default function ModelX() {
+  return (
+    <section className="p-2 md:bg-model-x-desktop bg-model-x-mobile bg-no-repeat  md:bg-center h-screen bg-cover overflow-hidden">
+      <div className="flex flex-col h-3/4 items-center mt-20 py-20 md:py-0">
+        <div id="hero-tex" className=" text-center font-Gotham p-8 md:p-8">
+          <h1 className="text-4xl md:-mt-6 font-medium text-gray-800">
+            Model X
+          </h1>
+          <a href="#" className="text-sm text-gray-700 underline">
+            Schedule a Test Drive
+          </a>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between gap-4 items-center md:p-11 p-4 mt-44 md:mt-72   ">
+          <a href="#">
+            <Button btnColor="bg-btn-bg text-white backdrop-blur opacity-95">
+              Buy Now
+            </Button>
+          </a>
+          <a href="#">
+            <Button btnColor="bg-white text-btn-bg backdrop-blur opacity-90">
+              Customer Order
+            </Button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
