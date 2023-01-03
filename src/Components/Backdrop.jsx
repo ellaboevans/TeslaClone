f5d21007ca68914e6511ef771790d20305085079
Evans <ellaboevans@gmail.com>
2023-01-03 16:30:39 +0000

    Remove Modal Import

--- src/Components/Backdrop.jsx
+++ src/Components/Backdrop.jsx
@@ -1,10 +1,9 @@
-import Modal from "./Modal";
 
 export default function Backdrop(props) {
   return (
     <div className="relative overflow-hidden ">
       <div
-        className=" w-full h-screen bg-gray-500 opacity-50 fixed  backdrop-blur-3xl  z-20"
+        className=" w-full h-screen bg-gray-400 opacity-50 fixed backdrop-blur-3xl blur-3xl   z-20"
         onClick={props.closeModal}
       ></div>
     </div>
