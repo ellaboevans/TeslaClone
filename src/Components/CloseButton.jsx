af656bbecdef10c36b752b495070ebfc97e15587
Evans <ellaboevans@gmail.com>
2023-01-05 13:33:21 +0000

    Add CloseModalBtn function to the component

---
+++ src/Components/CloseButton.jsx
@@ -0,0 +1,13 @@
+export default function CloseButton(props) {
+    console.log(props);
+    return (
+      <div className=" ml-20">
+        <span
+          className="text-4xl ml-36 cursor-pointer"
+          onClick={props.closeModalButton}
+        >
+          &times;
+        </span>
+      </div>
+    );
+}
