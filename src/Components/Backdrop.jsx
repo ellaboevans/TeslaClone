b0d696fa959cbda23474a6003a5be6e2e0ffd618
Evans Elabo <74984305+ellaboevans@users.noreply.github.com>
2023-01-26 11:28:58 +0000

    Destructure Backdrop Props

--- src/Components/Backdrop.jsx
+++ src/Components/Backdrop.jsx
@@ -1,10 +1,10 @@
 
-export default function Backdrop(props) {
+export default function Backdrop({closeModal}) {
   return (
     <div className="relative overflow-hidden ">
       <div
         className=" w-full h-screen bg-gray-400 opacity-50 fixed backdrop-blur-3xl blur-3xl   z-20"
-        onClick={props.closeModal}
+        onClick={closeModal}
       ></div>
     </div>
   );
